caa7316bc1b34124d1cd77dfe8f2817ff4159af3
zobayerhb <hosenzobayer55@gmail.com>
2025-01-01 21:30:49 +0600

    update job and show previous job

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -182,7 +182,7 @@ const MyPostedJobs = () => {
                           </button>
 
                           <Link
-                            to={`/update/1`}
+                            to={`/update/${job._id}`}
                             className="text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none"
                           >
                             <svg

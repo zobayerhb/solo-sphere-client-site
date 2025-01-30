f311cbf9bd0966cfeb1cdd2fb9772b83b0781423
zobayerhb <hosenzobayer55@gmail.com>
2025-01-30 22:23:34 +0600

    search, category & sorting

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -5,22 +5,35 @@ import axios from "axios";
 
 const AllJobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
 
   useEffect(() => {
+    const allJobs = async () => {
+      const { data } = await axios.get(
+        `${
+          import.meta.env.VITE_API_URL
+        }/all-jobs?filter=${filter}&search=${search}&sort=${sort}`
+      );
+      setJobs(data);
+    };
     allJobs();
-  }, []);
+  }, [filter, search, sort]);
 
-  const allJobs = async () => {
-    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/jobs`);
-    setJobs(data);
+  const handleReset = () => {
+    setFilter("");
+    setSearch("");
+    setSort("");
   };
-  console.log(jobs);
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
       <div>
         <div className="flex flex-col md:flex-row justify-center items-center gap-5 ">
           <div>
             <select
+              onChange={(e) => setFilter(e.target.value)}
+              value={filter}
               name="category"
               id="category"
               className="border p-4 rounded-lg"
@@ -38,6 +51,8 @@ const AllJobs = () => {
                 className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
                 type="text"
                 name="search"
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
                 placeholder="Enter Job Title"
                 aria-label="Enter Job Title"
               />
@@ -51,6 +66,8 @@ const AllJobs = () => {
             <select
               name="category"
               id="category"
+              onChange={(e) => setSort(e.target.value)}
+              value={sort}
               className="border p-4 rounded-md"
             >
               <option value="">Sort By Deadline</option>
@@ -58,7 +75,9 @@ const AllJobs = () => {
               <option value="asc">Ascending Order</option>
             </select>
           </div>
-          <button className="btn">Reset</button>
+          <button onClick={handleReset} className="btn">
+            Reset
+          </button>
         </div>
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {jobs.map((job) => (
